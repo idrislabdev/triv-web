607dbd0a4e4a3f2f372cf428f3d2ad261aa5c86d
mochamad rifai idris <inoreds@gmail.com>
2024-08-29 23:49:50 +0700

    update

---
+++ src/app/[lang]/home/cardano/page.tsx
@@ -0,0 +1,45 @@
+import React from 'react'
+import Footer from '@/@core/components/footer'
+import MainHeader from '@/@core/components/main-header'
+
+import ProductsEwalletSection from '@/@core/pages/products/ewallet-section'
+import ProductsInvestasiNowSection from '@/@core/pages/products/investasi-now-section'
+import ProductsTestimonyCarouselSection from '@/@core/pages/liverate/testimony-carousel-section'
+import ProductsAffliateSection from '@/@core/pages/products/affliate-section'
+import ProductsServiceHeaderSection from '@/@core/pages/products/service-header-section'
+import ProductsDeviceSection from '@/@core/pages/products/device-section'
+import ProductsRegulasiSection from '@/@core/pages/products/regulasi-section'
+import ProductsLayananSection from '@/@core/pages/products/layanan-section'
+import ProductsAverageSection from '@/@core/pages/products/average-section'
+
+import { getDictionariesCardano } from '@/app/dictionaries'
+import '@/styles/products.css'
+
+export default async function ServiceLiteCardanoPage({ params }: any) {
+    const {
+        header_section,
+        regulasi_section,
+        ewallet_section,
+        start_now_section,
+        layanan_section,
+        average_section,
+    } = await getDictionariesCardano(params.lang);
+    return (
+        <>
+            <MainHeader classText="header-white" lang={params.lang}/>
+                <main className='products-page sm:mobile-responsive light-theme'>
+                    <ProductsServiceHeaderSection objLang={header_section} />
+                    <ProductsDeviceSection />
+                    <ProductsRegulasiSection objLang={regulasi_section} />
+                    <ProductsLayananSection objLang={layanan_section} />
+                    <ProductsAverageSection objLang={average_section} />
+                    <ProductsAffliateSection objLang={null} />
+                    <ProductsEwalletSection objLang={ewallet_section}/>
+                    <ProductsInvestasiNowSection objLang={start_now_section} />
+                    <ProductsTestimonyCarouselSection />
+                </main>
+            <Footer />
+        </>
+  )
+}
+
--- src/app/dictionaries.tsx
+++ src/app/dictionaries.tsx
@@ -67,6 +67,90 @@ const dictionariesBitcoin: any = {
   id: () => import("./dictionaries/bitcoin/id.json").then((module) => module.default),
   en: () => import("./dictionaries/bitcoin/en.json").then((module) => module.default),
 };
+
+const dictionariesEthereum: any = {
+  id: () => import("./dictionaries/ethereum/id.json").then((module) => module.default),
+  en: () => import("./dictionaries/ethereum/en.json").then((module) => module.default),
+};
+
+const dictionariesStellar: any = {
+  id: () => import("./dictionaries/stellar/id.json").then((module) => module.default),
+  en: () => import("./dictionaries/stellar/en.json").then((module) => module.default),
+};
+
+const dictionariesRipple: any = {
+  id: () => import("./dictionaries/ripple/id.json").then((module) => module.default),
+  en: () => import("./dictionaries/ripple/en.json").then((module) => module.default),
+};
+
+const dictionariesEos: any = {
+  id: () => import("./dictionaries/eos/id.json").then((module) => module.default),
+  en: () => import("./dictionaries/eos/en.json").then((module) => module.default),
+};
+
+const dictionariesLitecoin: any = {
+  id: () => import("./dictionaries/litecoin/id.json").then((module) => module.default),
+  en: () => import("./dictionaries/litecoin/en.json").then((module) => module.default),
+};
+
+const dictionariesDash: any = {
+  id: () => import("./dictionaries/dash/id.json").then((module) => module.default),
+  en: () => import("./dictionaries/dash/en.json").then((module) => module.default),
+};
+
+const dictionariesTether: any = {
+  id: () => import("./dictionaries/tether/id.json").then((module) => module.default),
+  en: () => import("./dictionaries/tether/en.json").then((module) => module.default),
+};
+
+const dictionariesChainlink: any = {
+  id: () => import("./dictionaries/chainlink/id.json").then((module) => module.default),
+  en: () => import("./dictionaries/chainlink/en.json").then((module) => module.default),
+};
+
+const dictionariesDogecoin: any = {
+  id: () => import("./dictionaries/dogecoin/id.json").then((module) => module.default),
+  en: () => import("./dictionaries/dogecoin/en.json").then((module) => module.default),
+};
+
+const dictionariesBnb: any = {
+  id: () => import("./dictionaries/bnb/id.json").then((module) => module.default),
+  en: () => import("./dictionaries/bnb/en.json").then((module) => module.default),
+};
+
+const dictionariesMaticPolygon: any = {
+  id: () => import("./dictionaries/matic-polygon/id.json").then((module) => module.default),
+  en: () => import("./dictionaries/matic-polygon/en.json").then((module) => module.default),
+};
+
+
+const dictionariesShibainu: any = {
+  id: () => import("./dictionaries/shibainu/id.json").then((module) => module.default),
+  en: () => import("./dictionaries/shibainu/en.json").then((module) => module.default),
+};
+
+const dictionariesPolkadot: any = {
+  id: () => import("./dictionaries/polkadot/id.json").then((module) => module.default),
+  en: () => import("./dictionaries/polkadot/en.json").then((module) => module.default),
+};
+
+const dictionariesCardano: any = {
+  id: () => import("./dictionaries/cardano/id.json").then((module) => module.default),
+  en: () => import("./dictionaries/cardano/en.json").then((module) => module.default),
+};
+
+const dictionariesSolana: any = {
+  id: () => import("./dictionaries/solana/id.json").then((module) => module.default),
+  en: () => import("./dictionaries/solana/en.json").then((module) => module.default),
+};
+
+const dictionariesAxieinfinity: any = {
+  id: () => import("./dictionaries/axieinfinity/id.json").then((module) => module.default),
+  en: () => import("./dictionaries/axieinfinity/en.json").then((module) => module.default),
+};
+
+
+
 export const getDictionaryLogin = async (locale: any) => dictionariesLogin[locale]();  
 export const getDictionaryRegister = async (locale: any) => dictionariesRegister[locale]();  
 export const getDictionaryHome = async (locale: any) => dictionariesHome[locale]();  
@@ -81,3 +165,19 @@ export const getDictionariesProductsPulsa = async (locale: any) => dictionariesP
 export const getDictionariesProductsTokenListrik = async (locale: any) => dictionariesProductsTokenListrik[locale]();
 export const getDictionariesProductsTagihan = async (locale: any) => dictionariesProductsTagihan[locale]();
 export const getDictionariesBitcoin = async (locale: any) => dictionariesBitcoin[locale]();
+export const getDictionariesEtherum = async (locale: any) => dictionariesEthereum[locale]();
+export const getDictionariesStelllar = async (locale: any) => dictionariesStellar[locale]();
+export const getDictionariesRipple = async (locale: any) => dictionariesRipple[locale]();
+export const getDictionariesEos = async (locale: any) => dictionariesEos[locale]();
+export const getDictionariesLitecoin = async (locale: any) => dictionariesLitecoin[locale]();
+export const getDictionariesDash = async (locale: any) => dictionariesDash[locale]();
+export const getDictionariesTether = async (locale: any) => dictionariesTether[locale]();
+export const getDictionariesChainlink = async (locale: any) => dictionariesChainlink[locale]();
+export const getDictionariesDogeCoin = async (locale: any) => dictionariesDogecoin[locale]();
+export const getDictionariesBnb = async (locale: any) => dictionariesBnb[locale]();
+export const getDictionariesMaticPolgyon = async (locale: any) => dictionariesMaticPolygon[locale]();
+export const getDictionariesShibainu = async (locale: any) => dictionariesShibainu[locale]();
+export const getDictionariesPolkadot = async (locale: any) => dictionariesPolkadot[locale]();
+export const getDictionariesCardano = async (locale: any) => dictionariesCardano[locale]();
+export const getDictionariesSolana = async (locale: any) => dictionariesSolana[locale]();
+export const getDictionariesAxieinfinity = async (locale: any) => dictionariesAxieinfinity[locale]();
