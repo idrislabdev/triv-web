cc38c89073b4f4a0daca852d6067e1085888aad3
mochamad rifai idris <inoreds@gmail.com>
2024-08-30 06:25:47 +0700

    update

--- src/@core/pages/products/affliate-section/index.tsx
+++ src/@core/pages/products/affliate-section/index.tsx
@@ -11,19 +11,19 @@ const ProductsAffliateSection = (props: {objLang:any}) => {
             <Image src='/images/others/3d-gift.png' alt='3d-gift' width={0} height={0} sizes='100%'/>
             <div className='middle-subcontainer'>
                 <div className='sub-subcontainer'>
-                    <h5>Triv Affiliate</h5>
-                    <p>Dengan program Affiliate kami setiap downline melakukan transaksi, anda akan mendapatkan cashback 10% tanpa syarat!</p>
+                    <h5>{objLang.title_1}</h5>
+                    <p>{objLang.content_1}</p>
                 </div>
                 <div className='sub-subcontainer'>
-                    <h5>Undang teman dan saudara</h5>
-                    <p>Affiliate Triv tidak memiliki masa kadaluarsa dan berlaku seumur hidup, selama downline anda melakukan transaksi, maka anda akan tetap mendapatkan komisi tanpa batasan waktu.</p>
+                    <h5>{objLang.title_2}</h5>
+                    <p>{objLang.content_2}</p>
                 </div>
             </div>
             <div className='end-subcontainer'>
-                <h5>Ajak teman dan dapatkan tambahan penghasilan</h5>
+                <h5>{objLang.follow_text}</h5>
                 <div className='button-group'>
-                    <button>Affiliate Program</button>
-                    <button>Invite Friends</button>
+                    <button>{objLang.button_1}</button>
+                    <button>{objLang.button_2}</button>
                 </div>
             </div>
         </div>
