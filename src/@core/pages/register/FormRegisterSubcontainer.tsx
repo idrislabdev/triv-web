10518d2f9ac04faf6cc19d40567fbf6a52fbf441
mochamad rifai idris <inoreds@gmail.com>
2024-09-17 10:09:43 +0700

    update

---
+++ src/@core/pages/register/FormRegisterSubcontainer.tsx
@@ -0,0 +1,81 @@
+"use client"
+
+import { HideEyeIcon } from '@/@core/components/custom-icons'
+import Image from 'next/image'
+import React, { useState } from 'react'
+
+const FormRegisterSubcontainer = (props: {dictRegister:any, pekerjaans:string[]}) => {
+    const { dictRegister, pekerjaans } = props
+    const [dataPekerjaan, setDataPekerjaans] = useState(pekerjaans)
+    return (
+        <div className='form-subcontainer'>
+            <form>
+            <div className='form-name'>
+                <label>{dictRegister.name_form_text}</label>
+                <div className='form-input'>
+                <input type='text' className='color-1' placeholder={dictRegister.name_form_input_first_text}/>
+                <input type='text' className='color-1' placeholder={dictRegister.name_form_input_second_text}/>
+                </div>
+            </div>
+            <div className='form-email'>
+                <label>{dictRegister.email_text}</label>
+                <input type='email' className='color-1' 
+                    placeholder={dictRegister.email_text} 
+                    onInvalid={e => (e.target as HTMLInputElement).setCustomValidity("Please include an '@' in the email address ")} 
+                    required
+                />
+            </div>
+            <div className='form-password'>
+                <label>{dictRegister.password_text}</label>
+                <div className='form-input'>
+                <div className='group-input prepend'>
+                    <span className='prepend'><HideEyeIcon color={'#838899'} /></span>
+                    <input type='password' className='color-1' placeholder={dictRegister.password_text}/>
+                </div>
+                <div className='group-input prepend'>
+                    <span className='prepend'><HideEyeIcon color={'#838899'} /></span>
+                    <input type='password' className='color-1' placeholder={dictRegister.password_repead_text}/>
+                </div>
+                </div>
+            </div>
+            <div className='form-jobs'>
+                <label>{dictRegister.job_text}</label>
+                <select className='color-1'>
+                    {
+                        dataPekerjaan.map((item:string, index:number) => (
+                            <option value={item} key={index}>{item}</option>
+                        ))
+                    }
+                </select>
+            </div>
+            <div className='form-note'>
+                <p>{dictRegister.register_first_text} <span>{dictRegister.register_second_text}</span>{dictRegister.register_third_text} <span>{dictRegister.register_fourth_text}</span></p>
+            </div>
+            <button type='submit'>{dictRegister.register_text}</button>
+            </form>
+            <div className='sub-title-subcontainer'>
+                <h5>{dictRegister.welcome_text}</h5>
+                <p>{dictRegister.sub_welcome_text}</p>
+                <div className='terdaftar-diawasi'>
+                    <div className='img-wrapper'>
+                        <Image src='/images/otoritas-keuangans/bsi-logo.png' className='bsi-logo' alt='bsi' width={45.15} height={24.69}/>
+                    </div>
+                    <div className='img-wrapper'>
+                        <Image src='/images/otoritas-keuangans/cisa-logo.png' className='cisa-logo' alt='cisa' width={35.98} height={33.16}/>
+                    </div>
+                    <div className='img-wrapper'>
+                        <Image src='/images/otoritas-keuangans/cism-logo.png' className='cism-logo' alt='cism' width={79.01} height={30.33}/>
+                    </div>
+                    <div className='img-wrapper'>
+                        <Image src='/images/otoritas-keuangans/pci-logo.png' className='pci-logo' alt='pci' width={35.98} height={28.92}/>
+                    </div>
+                    <div className='img-wrapper'>
+                        <Image src='/images/otoritas-keuangans/cissp-logo.png' className='cissp-logo' alt='cissp' width={106.26} height={31.13}/>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default FormRegisterSubcontainer
--- src/app/[lang]/register/page.tsx
+++ src/app/[lang]/register/page.tsx
@@ -4,26 +4,10 @@ import Image from 'next/image'
 import { getDictionaryRegister } from '@/app/dictionaries';
 
 import '@/styles/register.css'
+import FormRegisterSubcontainer from '@/@core/pages/register/FormRegisterSubcontainer';
 
 export default async function RegisterPage({ params }: any) {
-    const {
-        greeting_text, 
-        entry_text, 
-        name_form_text, 
-        name_form_input_first_text, 
-        name_form_input_second_text,
-        email_text, 
-        password_text, 
-        password_repead_text, 
-        job_text,
-        register_first_text,
-        register_second_text,
-        register_third_text,
-        register_fourth_text,
-        welcome_text,
-        sub_welcome_text,
-        register_text,
-    } = await getDictionaryRegister(params.lang);
+    const dictRegister = await getDictionaryRegister(params.lang);
     const pekerjaans = [
       'Pelajar/Mahasiswa',
       'Karyawan Swasta',
@@ -42,75 +26,15 @@ export default async function RegisterPage({ params }: any) {
             <TrivIcon color={'#318AC6'}/>
           </div>
           <div className='title-subcontainer'>
-            <h1>{greeting_text}</h1>
-            <p>{entry_text}</p>
+            <h1>{dictRegister.greeting_text}</h1>
+            <p>{dictRegister.entry_text}</p>
           </div>
-          <div className='form-subcontainer'>
-            <div className='form-name'>
-              <label>{name_form_text}</label>
-              <div className='form-input'>
-                <input type='text' className='color-1' placeholder={name_form_input_first_text}/>
-                <input type='text' className='color-1' placeholder={name_form_input_second_text}/>
-              </div>
-            </div>
-            <div className='form-email'>
-              <label>{email_text}</label>
-              <input type='text' className='color-1' placeholder={email_text}/>
-            </div>
-            <div className='form-password'>
-              <label>{password_text}</label>
-              <div className='form-input'>
-                <div className='group-input prepend'>
-                    <span className='prepend'><HideEyeIcon color={'#838899'} /></span>
-                    <input type='password' className='color-1' placeholder={password_text}/>
-                </div>
-                <div className='group-input prepend'>
-                    <span className='prepend'><HideEyeIcon color={'#838899'} /></span>
-                    <input type='password' className='color-1' placeholder={password_repead_text}/>
-                </div>
-              </div>
-            </div>
-            <div className='form-jobs'>
-                <label>{job_text}</label>
-                <select className='color-1'>
-                    {
-                      pekerjaans.map((item:string, index:number) => (
-                        <option value={item} key={index}>{item}</option>
-                      ))
-                    }
-                </select>
-            </div>
-            <div className='form-note'>
-                <p>{register_first_text} <span>{register_second_text}</span>{register_third_text} <span>{register_fourth_text}</span></p>
-            </div>
-            <button>{register_text}</button>
-            <div className='sub-title-subcontainer'>
-                <h5>{welcome_text}</h5>
-                <p>{sub_welcome_text}</p>
-                <div className='terdaftar-diawasi'>
-                    <div className='img-wrapper'>
-                        <Image src='/images/otoritas-keuangans/bsi-logo.png' className='bsi-logo' alt='bsi' width={45.15} height={24.69}/>
-                    </div>
-                    <div className='img-wrapper'>
-                        <Image src='/images/otoritas-keuangans/cisa-logo.png' className='cisa-logo' alt='cisa' width={35.98} height={33.16}/>
-                    </div>
-                    <div className='img-wrapper'>
-                        <Image src='/images/otoritas-keuangans/cism-logo.png' className='cism-logo' alt='cism' width={79.01} height={30.33}/>
-                    </div>
-                    <div className='img-wrapper'>
-                        <Image src='/images/otoritas-keuangans/pci-logo.png' className='pci-logo' alt='pci' width={35.98} height={28.92}/>
-                    </div>
-                    <div className='img-wrapper'>
-                        <Image src='/images/otoritas-keuangans/cissp-logo.png' className='cissp-logo' alt='cissp' width={106.26} height={31.13}/>
-                    </div>
-                </div>
-            </div>
-          </div>
+          <FormRegisterSubcontainer dictRegister={dictRegister} pekerjaans={pekerjaans} />
         </div>
         <div className='right-subcontainer'>
           <div className='title-subcontainer'>
-            <h1>{welcome_text}</h1>
-            <p>{sub_welcome_text}</p>
+            <h1>{dictRegister.welcome_text}</h1>
+            <p>{dictRegister.sub_welcome_text}</p>
             <div className='terdaftar-diawasi'>
                 <Image src='/images/otoritas-keuangans/bsi-logo.png' className='bsi-logo' alt='bsi' width={24.88} height={13.61}/>
                 <Image src='/images/otoritas-keuangans/cisa-logo.png' className='cisa-logo' alt='cisa' width={19.83} height={18.27}/>
