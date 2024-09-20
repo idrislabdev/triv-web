7007f9c37c9b166e82a6fec58332f7629036c6b5
mochamad rifai idris <inoreds@gmail.com>
2024-09-20 08:46:07 +0700

    update

---
+++ src/@core/pages/login/index.tsx
@@ -0,0 +1,49 @@
+"use client"
+
+import { TrivIcon } from '@/@core/components/custom-icons'
+import React, { useEffect } from 'react'
+import FormSubcontainer from './FormSubcontainer'
+import Image from 'next/image'
+
+const Login = (props: {dictLogin:any}) => {
+    const { dictLogin } = props
+    useEffect(() => {
+        if (localStorage.getItem('mode') === 'dark-theme') {
+            document.body.classList.add("dark-theme");
+        }
+    })
+    return (
+        <div className='login-container'>
+            <div className='left-subcontainer'>
+            <div className='logo-subcontainer'>
+                <TrivIcon color={'#318AC6'}/>
+            </div>
+            <div className='title-subcontainer'>
+                <h1>{dictLogin.greeting_text}</h1>
+                <p>{dictLogin.entry_text}</p>
+            </div>
+            <FormSubcontainer 
+                keepmeText={dictLogin.keepme_text} 
+                notRobotText={dictLogin.not_robot_text} 
+                forgotText={dictLogin.forgot_text} 
+                newUserText={dictLogin.new_user_text} 
+                registerText={dictLogin.register_text} 
+            />
+            </div>
+            <div className='right-subcontainer'>
+                <div className='title-subcontainer'>
+                    <h1>{dictLogin.welcome_text}</h1>
+                    <p>{dictLogin.sub_welcome_text}</p>
+                </div>
+                <div className='image-subcontainer'>
+                    <Image src='/images/others/login-screen.png' alt='captcha' width={471} height={392}/>
+                </div>
+                <div className='logo-subcontainer'>
+                    <TrivIcon color={'#318AC6'}/>
+                </div>
+            </div>
+        </div>
+  )
+}
+
+export default Login
--- src/app/[lang]/login/page.tsx
+++ src/app/[lang]/login/page.tsx
@@ -5,18 +5,9 @@ import Image from 'next/image'
 
 import '@/styles/login.css'
 import FormSubcontainer from '@/@core/pages/login/FormSubcontainer';
+import Login from '@/@core/pages/login';
 export default async function LoginPage({ params }: any) {
-  const {
-    greeting_text, 
-    entry_text, 
-    keepme_text, 
-    not_robot_text, 
-    forgot_text,
-    new_user_text, 
-    register_text, 
-    welcome_text, 
-    sub_welcome_text
-  } = await getDictionaryLogin(params.lang);
+  const dictLogin = await getDictionaryLogin(params.lang);
 
   return (
     <html lang={params.lang}>
@@ -82,30 +73,7 @@ export default async function LoginPage({ params }: any) {
       </head>
       <body>
         <main className='login-page sm:mobile-responsive md:mobile-responsive light-theme'>
-          <div className='login-container'>
-            <div className='left-subcontainer'>
-              <div className='logo-subcontainer'>
-                <TrivIcon color={'#318AC6'}/>
-              </div>
-              <div className='title-subcontainer'>
-                <h1>{greeting_text}</h1>
-                <p>{entry_text}</p>
-              </div>
-              <FormSubcontainer keepmeText={keepme_text} notRobotText={not_robot_text} forgotText={forgot_text} newUserText={new_user_text} registerText={register_text} />
-            </div>
-            <div className='right-subcontainer'>
-              <div className='title-subcontainer'>
-                <h1>{welcome_text}</h1>
-                <p>{sub_welcome_text}</p>
-              </div>
-              <div className='image-subcontainer'>
-                <Image src='/images/others/login-screen.png' alt='captcha' width={471} height={392}/>
-              </div>
-              <div className='logo-subcontainer'>
-                <TrivIcon color={'#318AC6'}/>
-              </div>
-            </div>
-          </div>
+          <Login dictLogin={dictLogin}/>
         </main>
       </body>
     </html>
