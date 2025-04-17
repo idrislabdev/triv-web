464792e84e244cecce33feeedfdef5065b13945e
mochamad <inoreds@gmail.com>
2025-04-17 12:22:50 +0700

    update

--- src/@core/pages/stocks/dividen-section/index.tsx
+++ src/@core/pages/stocks/dividen-section/index.tsx
@@ -34,7 +34,8 @@ const StocksDividenSection = (props: {lang:string, objLang:any}) => {
                     <p className='fade-in entry-3'>{objLang.subtitle}</p>
                 </div>
                 <div className='fade-in entry-3'>
-                  <Marquee autoFill={true} speed={100} loop={0} gradient={true}>
+                  {/* <Marquee autoFill={true} speed={100} loop={0} gradient={true}> */}
+                  <Marquee speed={100} loop={0} gradient={true}>
                     <Image src='/images/logos/tesla.png' alt='tesla' width={0} height={0} sizes='100%' />
                     <Image src='/images/logos/amazon.png' alt='amazon' width={0} height={0} sizes='100%' />
                     <Image src='/images/logos/apple.png' alt='apple' width={0} height={0} sizes='100%' />
