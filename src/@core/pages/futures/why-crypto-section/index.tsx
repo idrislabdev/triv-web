8f019daef97e5af56fac508b4087e99b7d04f343
mochamad <inoreds@gmail.com>
2025-05-16 13:59:59 +0700

    update

--- src/@core/pages/futures/why-crypto-section/index.tsx
+++ src/@core/pages/futures/why-crypto-section/index.tsx
@@ -1,11 +1,16 @@
 "use client"
 
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { Navigation } from 'swiper/modules';
 import Image from 'next/image';
 
 const FuturesWhyCryptoSection = () => {
+    useEffect(() => {
+        setTimeout(() => {
+            window.dispatchEvent(new Event('resize'));
+        }, 100);
+    }, []);
   return (
     <section className='futures-why-crypto-section'>
         <div className='main-container'>
