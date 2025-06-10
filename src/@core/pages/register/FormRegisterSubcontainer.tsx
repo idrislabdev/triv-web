d24bc9813ec4f59b5b82ee1a0f3c21b53ac63d96
mochamad <inoreds@gmail.com>
2025-06-10 11:57:43 +0700

    update

--- src/@core/pages/register/FormRegisterSubcontainer.tsx
+++ src/@core/pages/register/FormRegisterSubcontainer.tsx
@@ -21,51 +21,49 @@ const FormRegisterSubcontainer = (props: {lang:string, dictRegister:any, pekerja
 
     return (
         <div className='form-subcontainer'>
-            <form>
-                <div className='form-name'>
-                    <label>{dictRegister.name_form_text}</label>
-                    <div className='form-input'>
-                    <input type='text' className='color-1' placeholder={dictRegister.name_form_input_first_text}/>
-                    <input type='text' className='color-1' placeholder={dictRegister.name_form_input_second_text}/>
+            <div className='form-name'>
+                <label>{dictRegister.name_form_text}</label>
+                <div className='form-input'>
+                <input type='text' className='color-1' placeholder={dictRegister.name_form_input_first_text}/>
+                <input type='text' className='color-1' placeholder={dictRegister.name_form_input_second_text}/>
+                </div>
+            </div>
+            <div className='form-email'>
+                <label>{dictRegister.email_text}</label>
+                <input type='email' className='color-1' 
+                    placeholder={dictRegister.email_text} 
+                    onInvalid={e => (e.target as HTMLInputElement).setCustomValidity("Please include an '@' in the email address ")} 
+                    required
+                />
+            </div>
+            <div className='form-password'>
+                <label>{dictRegister.password_text}</label>
+                <div className='form-input'>
+                    <div className='group-input prepend'>
+                        <span className='prepend'><HideEyeIcon color={'#838899'} /></span>
+                        <input type='password' className='color-1' placeholder={dictRegister.password_text}/>
+                    </div>
+                    <div className='group-input prepend'>
+                        <span className='prepend'><HideEyeIcon color={'#838899'} /></span>
+                        <input type='password' className='color-1' placeholder={dictRegister.password_repead_text}/>
                     </div>
                 </div>
-                <div className='form-email'>
-                    <label>{dictRegister.email_text}</label>
-                    <input type='email' className='color-1' 
-                        placeholder={dictRegister.email_text} 
-                        onInvalid={e => (e.target as HTMLInputElement).setCustomValidity("Please include an '@' in the email address ")} 
-                        required
-                    />
-                </div>
-                <div className='form-password'>
-                    <label>{dictRegister.password_text}</label>
-                    <div className='form-input'>
-                        <div className='group-input prepend'>
-                            <span className='prepend'><HideEyeIcon color={'#838899'} /></span>
-                            <input type='password' className='color-1' placeholder={dictRegister.password_text}/>
-                        </div>
-                        <div className='group-input prepend'>
-                            <span className='prepend'><HideEyeIcon color={'#838899'} /></span>
-                            <input type='password' className='color-1' placeholder={dictRegister.password_repead_text}/>
-                        </div>
-                    </div>
-                </div>
-                <div className='form-jobs'>
-                    <label>{dictRegister.job_text}</label>
-                    <select className={pekerjaan == '' ? `!text-neutral-500` : `color-1`} defaultValue={pekerjaan} onChange={changePekerjaan}>
-                        <option value="" disabled>{dictRegister.placeholder_job}</option>
-                        {
-                            dataPekerjaan.map((item:string, index:number) => (
-                                <option value={item} className='text-neutral-800' key={index}>{item}</option>
-                            ))
-                        }
-                    </select>
-                </div>
-                <div className='form-note'>
-                    <p>{dictRegister.register_first_text} <span>{dictRegister.register_second_text} </span>{dictRegister.register_third_text} <span> {dictRegister.register_fourth_text}</span></p>
-                </div>
-                <button onClick={() => onSubmit()}>{dictRegister.register_text}</button>
-            </form>
+            </div>
+            <div className='form-jobs'>
+                <label>{dictRegister.job_text}</label>
+                <select className={pekerjaan == '' ? `!text-neutral-500` : `color-1`} defaultValue={pekerjaan} onChange={changePekerjaan}>
+                    <option value="" disabled>{dictRegister.placeholder_job}</option>
+                    {
+                        dataPekerjaan.map((item:string, index:number) => (
+                            <option value={item} className='text-neutral-800' key={index}>{item}</option>
+                        ))
+                    }
+                </select>
+            </div>
+            <div className='form-note'>
+                <p>{dictRegister.register_first_text} <span>{dictRegister.register_second_text} </span>{dictRegister.register_third_text} <span> {dictRegister.register_fourth_text}</span></p>
+            </div>
+            <button onClick={() => onSubmit()}>{dictRegister.register_text}</button>
             <label className='has-user'>{dictRegister.has_account_1} <a>{dictRegister.has_account_2}</a></label>
             <div className='sub-title-subcontainer'>
                 <h5>{dictRegister.welcome_text}</h5>
