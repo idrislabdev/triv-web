38338e6a7f050043ff3e2baffe7f6cb9487c27ac
inoreds@gmail.com <inoreds@gmail.com>
2025-07-17 01:21:10 +0700

    udpate

--- src/@core/pages/reset-password/FormSubcontainer.tsx
+++ src/@core/pages/reset-password/FormSubcontainer.tsx
@@ -1,38 +1,58 @@
-"use client"
+'use client';
 
-import { HideEyeIcon, LockIcon, MailIcon } from '@/@core/components/custom-icons'
-import Image from 'next/image'
-import { useRouter } from 'next/navigation'
-import React, { useState } from 'react'
+import {
+  HideEyeIcon,
+  LockIcon,
+  MailIcon,
+} from '@/@core/components/custom-icons';
+import Image from 'next/image';
+import { useRouter } from 'next/navigation';
+import React, { useState } from 'react';
 
-const FormSubcontainer = (props: {lang:string, continueText:string, notRobotText:string}) => {
-    const { lang, continueText, notRobotText  } = props
-    const router = useRouter();
-    const [typePassword, setTypePassword] = useState('password');
+const FormSubcontainer = (props: {
+  lang: string;
+  continueText: string;
+  notRobotText: string;
+}) => {
+  const { lang, continueText, notRobotText } = props;
+  const router = useRouter();
+  const [typePassword, setTypePassword] = useState('password');
 
-    const onSendEmail = () => {
-        router.push(`/${lang}/reset-password/info`)
-    }
-    return (
-        <div className='form-subcontainer'>
-            <div className='group-input prepend-append'>
-                <span className='append'><MailIcon color={'#838899'} /></span>
-                <input type='text' className='color-1' placeholder='Email'/>
-            </div>
-            <div className='captcha-checkbox'>
-                <label className="checkbox-input">{notRobotText}
-                    <input type="checkbox" />
-                    <span className="checkmark"></span>
-                </label>
-                <div className='captcha-image'>
-                    <Image src='/images/others/captcha.png' alt='captcha' width={0} height={0} sizes='100%'/>
-                </div>
-            </div>
-            <div className='button-flex'>
-                <button onClick={() => onSendEmail()}>{continueText}</button>
-            </div>
+  const onSendEmail = () => {
+    router.push(`/${lang}/reset-password/info`);
+  };
+  return (
+    <div className="form-subcontainer">
+      <div className="form-group">
+        <label>Email</label>
+        <div className="group-input prepend-append">
+          <span className="append">
+            <MailIcon color={'#838899'} />
+          </span>
+          <input type="text" className="color-1" placeholder="Email" />
         </div>
-  )
-}
+      </div>
+      <div className="captcha-checkbox">
+        <label className="checkbox-input">
+          {notRobotText}
+          <input type="checkbox" />
+          <span className="checkmark"></span>
+        </label>
+        <div className="captcha-image">
+          <Image
+            src="/images/others/captcha.png"
+            alt="captcha"
+            width={0}
+            height={0}
+            sizes="100%"
+          />
+        </div>
+      </div>
+      <div className="button-flex">
+        <button onClick={() => onSendEmail()}>{continueText}</button>
+      </div>
+    </div>
+  );
+};
 
-export default FormSubcontainer
+export default FormSubcontainer;
--- src/@core/pages/reset-password/index.tsx
+++ src/@core/pages/reset-password/index.tsx
@@ -1,75 +1,85 @@
-"use client"
+'use client';
 
-import { TrivIcon } from '@/@core/components/custom-icons'
-import React, { useEffect } from 'react'
-import FormSubcontainer from './FormSubcontainer'
-import Image from 'next/image'
+import { TrivIcon } from '@/@core/components/custom-icons';
+import React, { useEffect } from 'react';
+import FormSubcontainer from './FormSubcontainer';
+import Image from 'next/image';
 
-const ResetPassword = (props: {lang:string, dictionaries:any}) => {
-    const { lang, dictionaries } = props
-    const heightResponsive = () => {
-        const elements = document.getElementsByClassName("login-page");
-        var element = elements[0];
-        if (element) {
-            if(window.innerHeight < 850) {
-                document.getElementsByClassName("login-page")[0].classList.add('medium-responsive')
-            } else {
-                document.getElementsByClassName("login-page")[0].classList.remove('medium-responsive')
-            }
-        }
+const ResetPassword = (props: { lang: string; dictionaries: any }) => {
+  const { lang, dictionaries } = props;
+  const heightResponsive = () => {
+    const elements = document.getElementsByClassName('login-page');
+    var element = elements[0];
+    if (element) {
+      if (window.innerHeight < 850) {
+        document
+          .getElementsByClassName('login-page')[0]
+          .classList.add('medium-responsive');
+      } else {
+        document
+          .getElementsByClassName('login-page')[0]
+          .classList.remove('medium-responsive');
+      }
     }
-    useEffect(()=> {
-        window.addEventListener('resize', ()=> {
-            heightResponsive();
-        })
-    }, [])
-    useEffect(() => {
-        heightResponsive();
-        if (localStorage.getItem('mode') === 'dark-theme') {
-            document.body.classList.add("dark-theme");
-        }
-    })
-    return (
-        <div className='reset-password-container'>
-            <div className='left-subcontainer'>
-                <div className='logo-subcontainer'>
-                    <TrivIcon color={'#318AC6'}/>
-                </div>
-                <div className='left-main-subcontainer'>
-                    <div className='title-subcontainer'>
-                        <h1>{dictionaries.greeting_text}</h1>
-                        <p>{dictionaries.entry_text}</p>
-                    </div>
-                    <FormSubcontainer 
-                        lang={lang}
-                        notRobotText={dictionaries.not_robot_text} 
-                        continueText={dictionaries.continue_text} 
-                    />
-                </div>
-            </div>
-            <div className='right-subcontainer'>
-                <div className='right-main-subcontainer'>
-                    <div className='title-subcontainer'>
-                        <h1>{dictionaries.welcome_text}</h1>
-                        <p>{dictionaries.sub_welcome_text}</p>
-                        <div className='terdaftar-diawasi'>
-                            <Image src='/images/otoritas-keuangans/bsi-logo.png' className='bsi-logo' alt='bsi' width={0} height={0} sizes='100%'/>
-                            <Image src='/images/otoritas-keuangans/cisa-logo.png' className='cisa-logo' alt='cisa' width={0} height={0} sizes='100%'/>
-                            <Image src='/images/otoritas-keuangans/cism-logo.png' className='cism-logo' alt='cism' width={0} height={0} sizes='100%'/>
-                            <Image src='/images/otoritas-keuangans/pci-logo.png' className='pci-logo' alt='pci' width={0} height={0} sizes='100%'/>
-                            <Image src='/images/otoritas-keuangans/cissp-logo.png' className='cissp-logo' alt='cissp' width={0} height={0} sizes='100%'/>
-                        </div>
-                    </div>
-                    <div className='image-subcontainer'>
-                        <Image src='/images/others/security-password.png' alt='captcha' width={0} height={0} sizes='100%'/>
-                    </div>
-                    <div className='logo-subcontainer'>
-                        <TrivIcon color={'#318AC6'}/>
-                    </div>
-                </div>
-            </div>
+  };
+  useEffect(() => {
+    window.addEventListener('resize', () => {
+      heightResponsive();
+    });
+  }, []);
+  useEffect(() => {
+    heightResponsive();
+    if (localStorage.getItem('mode') === 'dark-theme') {
+      document.body.classList.add('dark-theme');
+    }
+  });
+  return (
+    <div className="reset-password-container">
+      <div className="left-subcontainer">
+        <div className="logo-subcontainer">
+          <TrivIcon color={'#318AC6'} />
         </div>
-  )
-}
+        <div className="left-main-subcontainer">
+          <div className="title-subcontainer">
+            <h1>{dictionaries.greeting_text}</h1>
+            <p>{dictionaries.entry_text}</p>
+          </div>
+          <FormSubcontainer
+            lang={lang}
+            notRobotText={dictionaries.not_robot_text}
+            continueText={dictionaries.continue_text}
+          />
+        </div>
+      </div>
+      <div className="right-subcontainer">
+        <div className="right-main-subcontainer">
+          <div className="title-subcontainer">
+            <h1>{dictionaries.welcome_text}</h1>
+            <p>{dictionaries.sub_welcome_text}</p>
+            <div className="terdaftar-diawasi">
+              <div className="bsi-logo"></div>
+              <div className="cisa-logo"></div>
+              <div className="cism-logo"></div>
+              <div className="pci-logo"></div>
+              <div className="cissp-logo"></div>
+            </div>
+          </div>
+          <div className="image-subcontainer">
+            <Image
+              src="/images/others/security-password.png"
+              alt="captcha"
+              width={0}
+              height={0}
+              sizes="100%"
+            />
+          </div>
+          <div className="logo-subcontainer">
+            <TrivIcon color={'#318AC6'} />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default ResetPassword
+export default ResetPassword;
