f04343b7b4ff9e9ab193ca43346d604f0784e55b
inoreds@gmail.com <inoreds@gmail.com>
2025-07-18 05:48:28 +0700

    udpate

--- src/@core/pages/staking/calculator-section/index.tsx
+++ src/@core/pages/staking/calculator-section/index.tsx
@@ -1,85 +1,106 @@
-"use client"
+'use client';
 
-import { IStaking } from '@/@core/@types/interfaces'
-import Image from 'next/image'
-import React, { useEffect, useState } from 'react'
-import CurrencyInput from 'react-currency-input-field'
+import { IStaking } from '@/@core/@types/interfaces';
+import { formatterNumber, formatterNumber2 } from '@/@core/utils/general';
+import Image from 'next/image';
+import React, { useEffect, useState } from 'react';
+import CurrencyInput from 'react-currency-input-field';
 
-const StakingCalculatorSection = (props: {objLang:any, stakings: any, }) => {
-    const { objLang, stakings } = props
-    const [inputVal, setInputVal] = useState("0");
-    const [asset, setAsset] = useState<IStaking>(stakings.find((x:IStaking) => x.currency === 'ETH'))
-    const [yearlyReward, setYearlyReward] = useState(0);
-    const [monthlyReward, setMonthlyReward] = useState(0);
-    const [dailyReward, setDailyReward] = useState(0);
-    const selectAsset = (val: string) => {
-        setAsset(stakings.find((x:any) => x.currency === val))
-    }
+const StakingCalculatorSection = (props: { objLang: any; stakings: any }) => {
+  const { objLang, stakings } = props;
+  const [inputVal, setInputVal] = useState('0');
+  const [asset, setAsset] = useState<IStaking>(
+    stakings.find((x: IStaking) => x.currency === 'ETH')
+  );
+  const [yearlyReward, setYearlyReward] = useState(0);
+  const [monthlyReward, setMonthlyReward] = useState(0);
+  const [dailyReward, setDailyReward] = useState(0);
+  const selectAsset = (val: string) => {
+    setAsset(stakings.find((x: any) => x.currency === val));
+  };
 
-    useEffect(() => {
-        setYearlyReward((asset.apy / 100) * parseInt(inputVal))
-        setMonthlyReward(((asset.apy / 52) / 100) * parseInt(inputVal))
-        setDailyReward(((asset.apy / 365) / 100) * parseInt(inputVal))
-    }, [inputVal, asset.apy])
+  useEffect(() => {
+    setYearlyReward((asset.apy / 100) * parseInt(inputVal));
+    setMonthlyReward((asset.apy / 52 / 100) * parseInt(inputVal));
+    setDailyReward((asset.apy / 365 / 100) * parseInt(inputVal));
+  }, [inputVal, asset.apy]);
 
-    return (
-        <section className='staking-calculator-section'>
-            <div className='card-staking-calculator'>
-                <div className='title-container'>
-                    <h2>{objLang.title}</h2>
+  return (
+    <section className="staking-calculator-section">
+      <div className="card-staking-calculator">
+        <div className="title-container">
+          <h2>{objLang.title}</h2>
+        </div>
+        <div className="calculator-container">
+          <div className="top-subcontainer">
+            <div className="sub-subcontainer">
+              <div className="form-input">
+                <label>Stake Asset</label>
+                <select
+                  defaultValue={asset.currency}
+                  onChange={(e) => selectAsset(e.target.value)}
+                >
+                  {stakings.map((item: any, index: number) => (
+                    <option value={item.currency} key={index}>
+                      {item.currency}
+                    </option>
+                  ))}
+                </select>
+              </div>
+              <div className="form-input">
+                <label>Amount Stake</label>
+                <div className="group-input">
+                  <CurrencyInput
+                    value={inputVal}
+                    decimalSeparator=","
+                    groupSeparator="."
+                    onValueChange={(value) => setInputVal(value ? value : '0')}
+                    maxLength={12}
+                  />
+                  <span className="prepend">{asset.currency}</span>
                 </div>
-                <div className='calculator-container'>
-                    <div className='top-subcontainer'>
-                        <div className='sub-subcontainer'>
-                            <div className='form-input'>
-                                <label>Stake Asset</label>
-                                <select defaultValue={asset.currency} onChange={e => selectAsset(e.target.value)}>
-                                    {stakings.map((item:any, index:number) => (
-                                        <option value={item.currency} key={index}>{item.currency}</option>
-                                    ))}
-                                </select>
-                            </div>
-                            <div className='form-input'>
-                                <label>Amount Stake</label>
-                                <div className='group-input'>
-                                    <CurrencyInput
-                                        value={inputVal}
-                                        decimalSeparator="." 
-                                        groupSeparator=","
-                                        onValueChange={(value) => setInputVal(value ? value : "0")}
-                                    />
-                                    <span className='prepend'>{asset.currency}</span>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div className='middle-subcontainer'>
-                        <div className='sub-subcontainer'>
-                            <label>{asset.apy}<span>%</span></label>
-                            <span>Annual Yield (Est.)</span>
-                        </div>
-                        <div className='sub-subcontainer'>
-                            <label>{dailyReward.toFixed(7)} <span>{asset.currency}</span></label>
-                            <span>Daily Reward (Est.)</span>
-                        </div>
-                    </div>
-                    <div className='middle-subcontainer'>
-                        <div className='sub-subcontainer'>
-                            <label>{monthlyReward.toFixed(7)} <span>{asset.currency}</span></label>
-                            <span>Monthly Reward (Est.)</span>
-                        </div>
-                        <div className='sub-subcontainer'>
-                            <label>{yearlyReward.toFixed(7)} <span>{asset.currency}</span></label>
-                            <span>Yearly Reward (Est.)</span>
-                        </div>
-                    </div>
-                    <div className='bottom-subcontainer'>
-                        <p>{objLang.description}</p>
-                    </div>
-                </div>
+              </div>
             </div>
-        </section>
-    )
-}
+          </div>
+          <div className="middle-subcontainer">
+            <div className="sub-subcontainer">
+              <label>
+                {asset.apy}
+                <span>%</span>
+              </label>
+              <span>Annual Yield (Est.)</span>
+            </div>
+            <div className="sub-subcontainer">
+              <label>
+                {formatterNumber2(dailyReward.toFixed(2))}{' '}
+                <span>{asset.currency}</span>
+              </label>
+              <span>Daily Reward (Est.)</span>
+            </div>
+          </div>
+          <div className="middle-subcontainer">
+            <div className="sub-subcontainer">
+              <label>
+                {formatterNumber2(monthlyReward.toFixed(2))}{' '}
+                <span>{asset.currency}</span>
+              </label>
+              <span>Monthly Reward (Est.)</span>
+            </div>
+            <div className="sub-subcontainer">
+              <label>
+                {formatterNumber2(yearlyReward.toFixed(2))}{' '}
+                <span>{asset.currency}</span>
+              </label>
+              <span>Yearly Reward (Est.)</span>
+            </div>
+          </div>
+          <div className="bottom-subcontainer">
+            <p>{objLang.description}</p>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
 
-export default StakingCalculatorSection
+export default StakingCalculatorSection;
--- src/@core/utils/general.ts
+++ src/@core/utils/general.ts
@@ -1,52 +1,66 @@
-export const formatterNumber = (val:number) => {
-    if (!val) return 0;
-        return `${val}`.replace(/\B(?=(\d{3})+(?!\d))/g, ".").replace(/\.(?=\d{0,2}$)/g, ",");
-}
+export const formatterNumber = (val: number) => {
+  if (!val) return 0;
+  return `${val}`
+    .replace(/\B(?=(\d{3})+(?!\d))/g, '.')
+    .replace(/\.(?=\d{0,2}$)/g, ',');
+};
 
-export const formatterNumber2 = (val:string) => {   
-    if (!val) return 0;
-    if (parseFloat(val) < 1) return val.replace(".",",");
-    
-    return `${val}`.replace(/\B(?=(\d{3})+(?!\d))/g, ".").replace(/\.(?=\d{0,2}$)/g, ",");
-}
+export const formatterNumber2 = (val: string) => {
+  if (!val) return 0;
+  if (parseFloat(val) < 1) return val.replace('.', ',');
 
+  return `${val}`
+    .replace(/\B(?=(\d{3})+(?!\d))/g, '.')
+    .replace(/\.(?=\d{0,2}$)/g, ',');
+};
 
-export const nFormatter = (num:number, digits:number) => {
-    const lookup = [
-      { value: 1, symbol: "" },
-      { value: 1e3, symbol: "Rb" },
-      { value: 1e6, symbol: "Jt" },
-      { value: 1e9, symbol: "Mlr" },
-      { value: 1e12, symbol: "T" },
-      { value: 1e15, symbol: "Qa" },
-      { value: 1e18, symbol: "Qi" }
-    ];
-    const regexp = /\.0+$|(?<=\.[0-9]*[1-9])0+$/;
-    const item = lookup.findLast(item => num >= item.value);
-    return item ? (num / item.value).toFixed(digits).replace(regexp, "").concat(item.symbol) : "0";
-}
+export const formatterNumber3 = (val: number) => {
+  if (!val) return 0;
+  return `${val}`
+    .replace(/\B(?=(\d{3})+(?!\d))/g, ',')
+    .replace(/\.(?=\d{0,2}$)/g, '.');
+};
 
-export const nFormatter2 = (num:number, digits:number) => {
-    const lookup = [
-      { value: 1, symbol: "" },
-      { value: 1e3, symbol: "K" },
-      { value: 1e6, symbol: "M" },
-      { value: 1e9, symbol: "B" },
-      { value: 1e12, symbol: "T" },
-      { value: 1e15, symbol: "Qa" },
-      { value: 1e18, symbol: "Qi" }
-    ];
-    const regexp = /\.0+$|(?<=\.[0-9]*[1-9])0+$/;
-    const item = lookup.findLast(item => num >= item.value);
-    return item ? (num / item.value).toFixed(digits).replace(regexp, "").concat(item.symbol) : "0";
-}
+export const nFormatter = (num: number, digits: number) => {
+  const lookup = [
+    { value: 1, symbol: '' },
+    { value: 1e3, symbol: 'Rb' },
+    { value: 1e6, symbol: 'Jt' },
+    { value: 1e9, symbol: 'Mlr' },
+    { value: 1e12, symbol: 'T' },
+    { value: 1e15, symbol: 'Qa' },
+    { value: 1e18, symbol: 'Qi' },
+  ];
+  const regexp = /\.0+$|(?<=\.[0-9]*[1-9])0+$/;
+  const item = lookup.findLast((item) => num >= item.value);
+  return item
+    ? (num / item.value).toFixed(digits).replace(regexp, '').concat(item.symbol)
+    : '0';
+};
 
-export const formatPlusMinus = (val:number) => {
-  let returnVal = val.toString();;
+export const nFormatter2 = (num: number, digits: number) => {
+  const lookup = [
+    { value: 1, symbol: '' },
+    { value: 1e3, symbol: 'K' },
+    { value: 1e6, symbol: 'M' },
+    { value: 1e9, symbol: 'B' },
+    { value: 1e12, symbol: 'T' },
+    { value: 1e15, symbol: 'Qa' },
+    { value: 1e18, symbol: 'Qi' },
+  ];
+  const regexp = /\.0+$|(?<=\.[0-9]*[1-9])0+$/;
+  const item = lookup.findLast((item) => num >= item.value);
+  return item
+    ? (num / item.value).toFixed(digits).replace(regexp, '').concat(item.symbol)
+    : '0';
+};
+
+export const formatPlusMinus = (val: number) => {
+  let returnVal = val.toString();
   if (val > 0) {
-      returnVal = `+${returnVal}`
+    returnVal = `+${returnVal}`;
   } else if (val < 0) {
-      returnVal = `-${returnVal}`
+    returnVal = `-${returnVal}`;
   }
   return returnVal;
-}
+};
