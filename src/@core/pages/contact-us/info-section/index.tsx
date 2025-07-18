46145f5c3127323a0e831443c9dace039d0eadbf
inoreds@gmail.com <inoreds@gmail.com>
2025-07-18 13:56:44 +0700

    update

--- src/@core/pages/contact-us/info-section/index.tsx
+++ src/@core/pages/contact-us/info-section/index.tsx
@@ -1,50 +1,61 @@
-import { ArrowRightIcon, BuildingIcon, FaqIcon, MessageSquareIcon } from '@/@core/components/custom-icons'
-import React from 'react'
+import {
+  ArrowRightIcon,
+  BuildingIcon,
+  FaqIcon,
+  MessageSquareIcon,
+} from '@/@core/components/custom-icons';
+import React from 'react';
 
-const ContactUsInfoSection = (props: { objLang: any}) => {
-    const { objLang } = props
-    return (
-        <section className='contact-us-info-section'>
-            <div className='info-container'>
-                <div className='info-card'>
-                    <div className='card-header'>
-                        <div className='header-icon ask'>
-                            <FaqIcon color={'#fff'} />
-                        </div>
-                    </div>
-                    <div className='card-content'>
-                        <h5>{objLang.enquire.title}</h5>
-                        <p>{objLang.enquire.content}</p>
-                        <a>{objLang.enquire.link} <ArrowRightIcon color={'#fff'} /></a>
-                    </div>
-                </div>
-                <div className='info-card'>
-                    <div className='card-header'>
-                        <div className='header-icon live-chat'>
-                            <MessageSquareIcon color={'#fff'} />
-                        </div>
-                    </div>
-                    <div className='card-content'>
-                        <h5>{objLang.live_chat.title}</h5>
-                        <p>{objLang.live_chat.content}</p>
-                        <a>{objLang.live_chat.link} <ArrowRightIcon color={'#fff'} /></a>
-                    </div>
-                </div>
-                <div className='info-card'>
-                    <div className='card-header'>
-                        <div className='header-icon letter'>
-                            <BuildingIcon color={'#F8ECF5'} />
-                        </div>
-                    </div>
-                    <div className='card-content'>
-                        <h5>{objLang.by_post.title}</h5>
-                        <p>{objLang.by_post.content}</p>
-                        <a>{objLang.by_post.link} <ArrowRightIcon color={'#fff'} /></a>
-                    </div>
-                </div>
+const ContactUsInfoSection = (props: { objLang: any }) => {
+  const { objLang } = props;
+  return (
+    <section className="contact-us-info-section">
+      <div className="info-container">
+        <div className="info-card">
+          <div className="card-header">
+            <div className="header-icon ask">
+              <FaqIcon color={'#fff'} />
             </div>
-        </section>
-    )
-}
+          </div>
+          <div className="card-content">
+            <h5>{objLang.enquire.title}</h5>
+            <p>{objLang.enquire.content}</p>
+            <a href="mailto:help@triv.co.id">
+              {objLang.enquire.link} <ArrowRightIcon color={'#fff'} />
+            </a>
+          </div>
+        </div>
+        <div className="info-card">
+          <div className="card-header">
+            <div className="header-icon live-chat">
+              <MessageSquareIcon color={'#fff'} />
+            </div>
+          </div>
+          <div className="card-content">
+            <h5>{objLang.live_chat.title}</h5>
+            <p>{objLang.live_chat.content}</p>
+            <a href="https://triv.freshdesk.com/support/home" target="_blank">
+              {objLang.live_chat.link} <ArrowRightIcon color={'#fff'} />
+            </a>
+          </div>
+        </div>
+        <div className="info-card">
+          <div className="card-header">
+            <div className="header-icon letter">
+              <BuildingIcon color={'#F8ECF5'} />
+            </div>
+          </div>
+          <div className="card-content">
+            <h5>{objLang.by_post.title}</h5>
+            <p>{objLang.by_post.content}</p>
+            <a href="tel:+62140200828">
+              {objLang.by_post.link} <ArrowRightIcon color={'#fff'} />
+            </a>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
 
-export default ContactUsInfoSection
+export default ContactUsInfoSection;
