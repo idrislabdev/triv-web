bf8779485fab33bbf34822924d7ea401795be2ae
inoreds@gmail.com <inoreds@gmail.com>
2025-07-18 14:20:53 +0700

    update

--- src/@core/pages/contact-us/header-section/index.tsx
+++ src/@core/pages/contact-us/header-section/index.tsx
@@ -1,16 +1,25 @@
-const ContactUsHeaderSection = (props: { objLang: any}) => {
-    const { objLang } = props
-    return (
-        <section className='contact-us-header-section'>
-            <div className='header-container'>
-                <h2>{objLang.title}</h2>
-                <div className='header-subcontainer'>
-                    <p>{objLang.description}</p>
-                    <p>{objLang.to} <span>help@triv.co.id</span> | <span>Contact our CEO</span></p>
-                </div>
-            </div>
-        </section>
-    )
-}
+const ContactUsHeaderSection = (props: { objLang: any }) => {
+  const { objLang } = props;
+  return (
+    <section className="contact-us-header-section">
+      <div className="header-container">
+        <h2>{objLang.title}</h2>
+        <div className="header-subcontainer">
+          <p>{objLang.description}</p>
+          <p>
+            {objLang.to}{' '}
+            <span>
+              <a href="mailto:help@triv.co.id">help@triv.co.id</a>
+            </span>{' '}
+            |{' '}
+            <span>
+              <a href="mailto:help@triv.co.id">Contact our CEO</a>
+            </span>
+          </p>
+        </div>
+      </div>
+    </section>
+  );
+};
 
-export default ContactUsHeaderSection
+export default ContactUsHeaderSection;
