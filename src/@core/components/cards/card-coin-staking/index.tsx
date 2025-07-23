97127b1e4a45b9f18d51d985eaf102e66a583ff4
inoreds@gmail.com <inoreds@gmail.com>
2025-07-23 12:11:19 +0700

    update

--- src/@core/components/cards/card-coin-staking/index.tsx
+++ src/@core/components/cards/card-coin-staking/index.tsx
@@ -1,24 +1,33 @@
-import { IStaking } from '@/@core/@types/interfaces'
-import Image from 'next/image'
-import React from 'react'
+import { IStaking } from '@/@core/@types/interfaces';
+import Image from 'next/image';
+import React from 'react';
 
-const CardCoinStaking = (props: {item:IStaking, objLang:any}) => {
-    const { item, objLang } = props
-    return (
-        <div className='card-coin-staking-new'>
-            <div className='card-coin-desc'>
-                <label>{item.label}</label>
-                <span className='span-text'>{objLang.text_card_1} {item.label} <span> {objLang.text_card_2} {item.apy}% / </span>{objLang.text_card_3}</span>
-                <span className='span-label'>{objLang.text_card_4} {item.apy}%</span>
-            </div>
-            <div className='card-coin-logo'>
-                <Image src={item.icon_url} alt={item.label}width={226} height={101}/>
-            </div>
-            <div className='card-button'>
-                <a>Stake Now</a>
-            </div>
-        </div>
-  )
-}
+const CardCoinStaking = (props: { item: IStaking; objLang: any }) => {
+  const { item, objLang } = props;
+  return (
+    <div className="card-coin-staking-new">
+      <div className="card-coin-desc">
+        <label>{item.label}</label>
+        <span className="span-text">
+          {objLang.text_card_1} {item.label}{' '}
+          <span>
+            {' '}
+            {objLang.text_card_2} {item.apy}% /{' '}
+          </span>
+          {objLang.text_card_3}
+        </span>
+        <span className="span-label">
+          {objLang.text_card_4} {item.apy}% / {objLang.text_card_5}
+        </span>
+      </div>
+      <div className="card-coin-logo">
+        <Image src={item.icon_url} alt={item.label} width={226} height={101} />
+      </div>
+      <div className="card-button">
+        <a>Stake Now</a>
+      </div>
+    </div>
+  );
+};
 
-export default CardCoinStaking
+export default CardCoinStaking;
